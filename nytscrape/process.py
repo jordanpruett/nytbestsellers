006faddca49ac2afb52488340d2180fa2c1b9ada
jordan <jordanpruett@gmail.com>
2021-12-08 15:39:11 -0800

    move scripts into nytscrape dir

---
+++ nytscrape/process.py
@@ -0,0 +1,8 @@
+"""
+Methods for processing data extracted from Hawes bestseller PDFs.
+
+We wish to yield three related .csv files:
+1) a table of all individual entries over the whole history of the list
+2) a table of individual titles, with a sum of total weeks on the list
+3) a table of titles matched to HathiTrust
+"""
